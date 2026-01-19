a62e86506db9cd5e455201637c7d7b98c4401bda
Yaryna Laiter <91200119+yaryna-laiter@users.noreply.github.com>
2026-01-19 20:46:02 +0100

    style: fix code formatting and whitespace

--- src/Recalution.Application/Interfaces/Repositories/IDeckRepository.cs
+++ src/Recalution.Application/Interfaces/Repositories/IDeckRepository.cs
@@ -5,5 +5,5 @@ namespace Recalution.Application.Interfaces.Repositories;
 public interface IDeckRepository : IRepository<Deck>
 {
     Task<IReadOnlyCollection<Deck>> GetDeckByUserId(Guid userId);
-    Task<bool> DeckExistsAsync(string name,  Guid ownerId);
+    Task<bool> DeckExistsAsync(string name, Guid ownerId);
 }
--- src/Recalution.Domain/Entities/FlashCard.cs
+++ src/Recalution.Domain/Entities/FlashCard.cs
@@ -5,7 +5,7 @@ public class FlashCard
     public Guid Id { get; set; }
     public required string Question { get; set; }
     public required string Answer { get; set; }
-    
+
     public Guid DeckId { get; set; }
     public Deck Deck { get; set; } = null!;
 }
