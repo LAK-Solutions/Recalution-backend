d25d6d3d0e65e0436ccb48786edef5d5448a6d71
Yaryna Laiter <91200119+yaryna-laiter@users.noreply.github.com>
2026-01-20 19:48:25 +0100

    feat(api): Add endpoint to get flash cards by deck

--- src/Recalution.Infrastructure/DependencyInjection.cs
+++ src/Recalution.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,8 @@
 using Microsoft.Extensions.DependencyInjection;
 using Recalution.Application.Interfaces;
+using Recalution.Application.Interfaces.Repositories;
 using Recalution.Application.Interfaces.Services;
+using Recalution.Infrastructure.Repositories;
 using Recalution.Infrastructure.Services;
 
 namespace Recalution.Infrastructure;
@@ -9,6 +11,10 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
+        services.AddScoped<IDeckRepository, DeckRepository>();
+        services.AddScoped<IFlashCardRepository, FlashCardRepository>();
+
+
         services.AddScoped<IAuthService, AuthService>();
         services.AddScoped<IJwtService, JwtService>();
 
